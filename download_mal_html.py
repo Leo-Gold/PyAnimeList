b3d996d9704952728d33061e3f7726836282b169
Maksim Borodkin <m9153055991@gmail.com>
2025-04-21 19:59:22 +0300

    getted username for settings

--- download_mal_html.py
+++ download_mal_html.py
@@ -2,39 +2,39 @@ import requests
 
 import settings
 
+
 def url_anime_list(username, status_id, filter=''):
-    response = None
     try:
         url = f"https://myanimelist.net/animelist/{username}"
         response = requests.get(url, params={'status': status_id, 'tag': filter})
         response.raise_for_status()
         return response
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP ошибка: {http_err} - Код статуса: {response.status_code}")
-    except requests.exceptions.ConnectionError:
-        print("Ошибка соединения. Проверьте соединение с интернетом.")
-    except requests.exceptions.Timeout:
-        print("Превышено время ожидания.")
-    except requests.exceptions.RequestException as err:
-        print(f"Произошла ошибка: {err}")
     except Exception as e:
         print(f"Непредвиденная ошибка: {e}")
+    return None
 
 
-def main():
-    name = input('Введите ваш ник на My Anime List: ')
+def main(username):
     for status in settings.STATUS_SELECTED:
-        content = url_anime_list(name, status.get('id')).text
-        save_content(content, status.get('name'))
+        response = url_anime_list(username, status.get('id'))
+        save_content(response.text, status.get('name'))
 
 
-def save_content(content, file_name):
+def save_content(content, name):
     settings.DATA_SOURCE_MYANIMELIST.mkdir(parents=True, exist_ok=True)
-    file = f"{settings.DATA_SOURCE_MYANIMELIST}/{file_name}.html"
-    with open(file, "w", encoding="utf-8") as file:
-        file.write(content)
-        print(f"Страница сохранена как {file}")
+
+    fullfile_path = settings.DATA_SOURCE_MYANIMELIST / f'{name}.html'
+
+    with open(fullfile_path, "w", encoding="utf-8") as f:
+        f.write(content)
+        print(f"Страница сохранена в {fullfile_path}")
+
+
 
 
 if __name__ == "__main__":
-    main()
+    username = settings.MAL_USERNAME
+    if not username:
+        print('Не указано имя пользователя')
+        exit(1)
+    main(username)
--- download_shikimori_history.py
+++ download_shikimori_history.py
@@ -4,15 +4,15 @@ import requests
 
 import settings
 
-def main():
-    userInfo = get_user_info()
+
+def main(username):
+    userInfo = get_user_info(username)
 
     pass
 
 
-def get_user_info():
+def get_user_info(username):
     response = None
-    username = input('Введите ваш ник на ShikiMori: ')
 
     try:
         url = f"https://shikimori.one/api/users/{username}/info"
@@ -22,22 +22,20 @@ def get_user_info():
                                          'grant_type': 'authorization_code',
                                          'client_id': settings.SHIKIMORI_CLIENT_ID,
                                          'client_secret': settings.SHIKIMORI_CLIENT_SECRET,
-                                         'code':'AUTORIZATION_CODE',
-                                         'redirect_uri':'urn:ietf:wg:oauth:2.0:oob'}
+                                         'code': 'AUTORIZATION_CODE',
+                                         'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob'}
                                 )
         response.raise_for_status()
         return response.json()
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP ошибка: {http_err} - Код статуса: {response.status_code}")
-    except requests.exceptions.ConnectionError:
-        print("Ошибка соединения. Проверьте соединение с интернетом.")
-    except requests.exceptions.Timeout:
-        print("Превышено время ожидания.")
-    except requests.exceptions.RequestException as err:
-        print(f"Произошла ошибка: {err}")
     except Exception as e:
         print(f"Непредвиденная ошибка: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    username = settings.SHIKIMORI_USERNAME
+
+    if not username:
+        print('Не указано имя пользователя')
+        exit(1)
+
+    main(username)
